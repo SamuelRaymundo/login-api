7d0b555b85abc7fe9fceccadcd3cdfa06ee1e93f
Samuel Raymundo <161658735+SamuelRaymundo@users.noreply.github.com>
2024-12-04 06:28:09 -0300

    first commit

---
+++ src/main/java/org/samuelraymundo/login_spring_security_jwt/LoginSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package org.samuelraymundo.login_spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSpringSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/samuelraymundo/login_spring_security_jwt/LoginSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samuelraymundo.login_spring_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
